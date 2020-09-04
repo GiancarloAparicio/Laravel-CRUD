5beb14b0a2704da59c8a7fa3d4e45eb9b827c9f6
GiancarloAparicio <zairo2542@gmail.com>
2020-09-03 21:48:54 -0500

    Laravel web start

---
+++ resources/views/components/table.blade.php
@@ -0,0 +1,25 @@
+<div class="card">
+    <table class="table ">
+      <thead class="thead-dark">
+        <tr>
+          <th scope="col">#</th>
+          <th scope="col">Task</th>
+          <th scope="col">Description</th>
+          <th scope="col">Create at</th>
+          <th scope="col"> Options </th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <th scope="row">1</th>
+          <td>Mark</td>
+          <td>Otto</td>
+          <td>@mdo</td>
+          <td>
+              <a href="" class="btn btn-warning">Update</a>
+              <a href="" class="btn btn-danger">Delete</a>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+</div>
